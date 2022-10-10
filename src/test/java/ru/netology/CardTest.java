4e82a35e39ff5a5b65a25f42f20f4cf501e6101a
Scriper.irk <scriper.irk@gmail.com>
2022-10-10 23:25:36 +0800

    Step 8

--- src/test/java/ru/netology/CardTest.java
+++ src/test/java/ru/netology/CardTest.java
@@ -22,6 +22,7 @@ public class CardTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
+
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
