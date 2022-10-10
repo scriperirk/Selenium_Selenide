5b7785dea7a217d6fc37a4c90970b7d579d5222c
Scriper.irk <scriper.irk@gmail.com>
2022-10-10 19:34:45 +0800

    Step 5

--- src/test/java/ru/netology/CardTest.java
+++ src/test/java/ru/netology/CardTest.java
@@ -44,4 +44,46 @@ public class CardTest {
 
         Assertions.assertEquals(expectedText, actualText);
     }
+
+    @Test
+    void shouldCardTestName() {
+
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Fafyfcjd Fynjy");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79994008600");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+
+        String actualText = driver.findElement(By.cssSelector("[data-test-id=name].input_invalid .input__sub")).getText().trim();
+        String expectedText = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+
+        Assertions.assertEquals(expectedText, actualText);
+    }
+
+    @Test
+    void shouldCardTestPhone() {
+
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Афанасов Антон");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+7999400860");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+
+        String actualText = driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")).getText().trim();
+        String expectedText = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+
+        Assertions.assertEquals(expectedText, actualText);
+    }
+
+    @Test
+    void shouldCardTestAgree() {
+
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Афанасов Антон");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79994008600");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+
+        String actualText = driver.findElement(By.cssSelector("[data-test-id='agreement'].input_invalid .checkbox__text")).getText();
+        String expectedText = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
+
+        Assertions.assertEquals(expectedText, actualText);
+    }
 }
